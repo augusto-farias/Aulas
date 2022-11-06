ba854dea47ede3f17ddf24cc889f0d5e8924a825
augusto-farias <farias4034@outlook.com>
2022-11-06 19:08:57 -0300

    first commit

---
+++ comidas.php
@@ -0,0 +1,26 @@
+<?php
+{
+    Temas: Comidas{
+        tipos{
+            Arroz Carreteiro
+            Arroz Doce
+            Baiao de Dois
+            Bife acebolado
+            Figado
+            Carne de Sol
+            Frango Frito
+            File de Frango
+            Cozido
+            Assado de Panela
+            Bisteca Suina
+            Charque Guisada
+            Carne Moida com Legumes
+            Contra File na manteiga
+            Bife ao Molho
+            Carne Guisada com Legumes
+            File de Peixe a Dore
+            File de Peixe Grelhado
+        }
+    }
+}
+?>
